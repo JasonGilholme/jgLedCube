6ab97aa2d07d69bf7e25f64c93f2a5d891517522
Jason Gilholme <jasongilholme@gmail.com>
2016-09-30 12:30:49 +1000

    extern const declare in .h and const definition in .cpp.  Otherwise I get duplicate objects, one external for everything and one internal to the cpp

--- jgLedCube/core.cpp
+++ jgLedCube/core.cpp
@@ -15,6 +15,27 @@
 namespace jgLedCube {
     namespace core {
 
+        const char* cubeName = LED_CUBE_NAME;
+        const double cubeUid = LED_CUBE_UID;
+        const double cubeVersion = LED_CUBE_VERSION;
+
+        // CUBE CONFIGURATION
+        const uint8_t xDimension = LED_CUBE_X_DIMENSION;
+        const uint8_t yDimension = LED_CUBE_Y_DIMENSION;
+        const uint8_t zDimension = LED_CUBE_Z_DIMENSION;
+        const uint8_t nChannels = LED_CUBE_N_CHANNELS;
+
+        // VARS FOR INTERNAL WORKINGS
+        const uint8_t xyDimension = LED_CUBE_XY_DIMENSION;
+        const uint8_t xyzDimension = LED_CUBE_XYZ_DIMENSION;
+        const uint8_t modulationBits = LED_CUBE_MODULATION_BITS;
+        const uint8_t maxIntensity = LED_CUBE_MAX_INTENSITY;
+        const uint8_t modBlockSize = LED_CUBE_MODULATION_BLOCK_SIZE;
+        const uint8_t dataArraySize = LED_CUBE_DATA_ARRAY_SIZE;
+
+        // THE MAIN DATA ARRAY
+        uint8_t dataArray[dataArraySize] = {};
+
         void setBit(uint8_t &b, const uint8_t &n, const uint8_t &v) {
             b ^= (-v ^ b) & (1 << n);
         }
@@ -95,8 +116,8 @@ namespace jgLedCube {
         }
 
         void printConfig() {
-            std::cout << "CUBE_NAME: "              << LED_CUBE_NAME              << std::endl;
-            std::cout << "CUBE_UID: "               << LED_CUBE_UID              << std::endl;
+            std::cout << "CUBE_NAME: "              << LED_CUBE_NAME                    << std::endl;
+            std::cout << "CUBE_UID: "               << LED_CUBE_UID                     << std::endl;
             std::cout << "CUBE_VERSION: "           << LED_CUBE_VERSION                 << std::endl;
             std::cout << "X_DIMENSION: "            << LED_CUBE_X_DIMENSION             << std::endl;
             std::cout << "Y_DIMENSION: "            << LED_CUBE_Y_DIMENSION             << std::endl;
--- jgLedCube/core.h
+++ jgLedCube/core.h
@@ -3,38 +3,32 @@
 #ifndef JGLEDCUBE_CORE_H
 #define JGLEDCUBE_CORE_H
 
-#define LED_CUBE_XY_DIMENSION           LED_CUBE_X_DIMENSION * LED_CUBE_Y_DIMENSION
-#define LED_CUBE_XYZ_DIMENSION          LED_CUBE_XY_DIMENSION * LED_CUBE_Y_DIMENSION
-#define LED_CUBE_MODULATION_BITS        4
-#define LED_CUBE_MAX_INTENSITY          15
-#define LED_CUBE_MODULATION_BLOCK_SIZE  LED_CUBE_XYZ_DIMENSION / 8.0 * LED_CUBE_N_CHANNELS
-#define LED_CUBE_DATA_ARRAY_SIZE        LED_CUBE_MODULATION_BLOCK_SIZE * LED_CUBE_MODULATION_BITS
+
+
+#define LED_CUBE_XY_DIMENSION LED_CUBE_X_DIMENSION * LED_CUBE_Y_DIMENSION
+#define LED_CUBE_XYZ_DIMENSION LED_CUBE_XY_DIMENSION * LED_CUBE_Y_DIMENSION
+#define LED_CUBE_MODULATION_BITS 4
+#define LED_CUBE_MAX_INTENSITY 15
+#define LED_CUBE_MODULATION_BLOCK_SIZE LED_CUBE_XYZ_DIMENSION / 8.0 * LED_CUBE_N_CHANNELS
+#define LED_CUBE_DATA_ARRAY_SIZE LED_CUBE_MODULATION_BLOCK_SIZE * LED_CUBE_MODULATION_BITS
 
 #include <stdint.h>
 
 namespace jgLedCube {
     namespace core {
-
-        static const char* cubeName = LED_CUBE_NAME;
-        static const double cubeUid = LED_CUBE_UID;
-        static const double cubeVersion = LED_CUBE_VERSION;
+        // CUBE IDENTIFICATION
+        extern const char* cubeName;
+        extern const double cubeUid;
+        extern const double cubeVersion;
 
         // CUBE CONFIGURATION
-        static const uint8_t xDimension = LED_CUBE_X_DIMENSION;
-        static const uint8_t yDimension = LED_CUBE_Y_DIMENSION;
-        static const uint8_t zDimension = LED_CUBE_Z_DIMENSION;
-        static const uint8_t nChannels = LED_CUBE_N_CHANNELS;
-
-        // VARS FOR INTERNAL WORKINGS
-        static const uint8_t xyDimension = LED_CUBE_XY_DIMENSION;
-        static const uint8_t xyzDimension = LED_CUBE_XYZ_DIMENSION;
-        static const uint8_t modulationBits = LED_CUBE_MODULATION_BITS;
-        static const uint8_t maxIntensity = LED_CUBE_MAX_INTENSITY;
-        static const uint8_t modBlockSize = LED_CUBE_MODULATION_BLOCK_SIZE;
-        static const uint8_t dataArraySize = LED_CUBE_DATA_ARRAY_SIZE;
+        extern const uint8_t xDimension;
+        extern const uint8_t yDimension;
+        extern const uint8_t zDimension;
+        extern const uint8_t nChannels;
 
         // THE MAIN DATA ARRAY
-        static uint8_t dataArray[dataArraySize] = {};
+        extern uint8_t dataArray[];
 
         // KEY FUNCTIONS FOR SETTING DATA ON THE CUBE
         void setLed(uint8_t x, uint8_t y, uint8_t z, uint8_t r, uint8_t g, uint8_t b);
