824744e81ba272cb9f5a2a71869b30c546e1b975
Jason Gilholme <jasongilholme@gmail.com>
2017-04-02 13:14:26 +1000

    Setup catch testing alongside gtests.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -6,10 +6,16 @@ if (${CUBE_TARGET_PLATFORM} STREQUAL desktop)
     #
     find_package(GTest REQUIRED)
     include_directories(${GTEST_INCLUDE_DIR})
+    define_property(GLOBAL PROPERTY GTEST_SOURCES BRIEF_DOCS "Global list of test sources" FULL_DOCS "Global list of test sources")
+    define_property(GLOBAL PROPERTY GTEST_LIBS BRIEF_DOCS "Global list of test libs" FULL_DOCS "Global list of test libs")
 
-    # Set some global properties to gather all the test sources and libs for.
-    define_property(GLOBAL PROPERTY TEST_SOURCES BRIEF_DOCS "Global list of test sources" FULL_DOCS "Global list of test sources")
-    define_property(GLOBAL PROPERTY TEST_LIBS BRIEF_DOCS "Global list of test libs" FULL_DOCS "Global list of test libs")
+    #
+    # setup catch test
+    #
+    include_directories($ENV{REZ_CATCH_ROOT}/include)
+    define_property(GLOBAL PROPERTY CATCH_TEST_SOURCES BRIEF_DOCS "Global list of test sources" FULL_DOCS "Global list of test sources")
+    define_property(GLOBAL PROPERTY CATCH_TEST_LIBS BRIEF_DOCS "Global list of test libs" FULL_DOCS "Global list of test libs")
+
 
     #
     # find & setup Qt
--- rezenv.sh
+++ rezenv.sh
@@ -1,1 +1,1 @@
-rez env -b boost-1.63 qt-5 googletest
+rez env -b boost-1.63 qt-5 googletest catch
